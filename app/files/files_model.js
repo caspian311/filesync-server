6e881b6efef8f9dbcf20306918bad556915c697a
Matt Todd <matt.c.todd@gmail.com>
2013-02-09 11:28:30 -0600

    slight refactoring to allow for the model to take different directories [#2]

--- app/files/files_model.js
+++ app/files/files_model.js
@@ -4,49 +4,54 @@
 
    var FILES_DIRECTORY = "/tmp/uploads/"
 
-   var _get_all_files = function(callback) {
-      fs.readdir(FILES_DIRECTORY, function(err, files) {
-         if (err)
-            throw err
+   var FileModel = function(files_directory) {
+      if (!files_directory) {
+         files_directory = FILES_DIRECTORY
+      }
 
-         if (!files) {
-            files = []
-         }
-         callback(files)
-      })
-   }
-
-   var _upload = function(uploaded_file, callback) {
-      fs.readFile(uploaded_file.path, function(err, data) {
-         if (err)
-            throw err
-
-         var newPath = FILES_DIRECTORY + uploaded_file.filename
-         fs.writeFile(newPath, data, function(err) {
+      var _get_all_files = function(callback) {
+         fs.readdir(FILES_DIRECTORY, function(err, files) {
             if (err)
                throw err
 
+            if (!files) {
+               files = []
+            }
+            callback(files)
+         })
+      }
+
+      var _upload = function(uploaded_file, callback) {
+         fs.readFile(uploaded_file.path, function(err, data) {
+            if (err)
+               throw err
+
+            var newPath = FILES_DIRECTORY + uploaded_file.filename
+            fs.writeFile(newPath, data, function(err) {
+               if (err)
+                  throw err
+
+               callback()
+            })
+         })
+      }
+
+      var _delete_file = function(file_to_delete, callback) {
+         fs.unlink(FILES_DIRECTORY + file_to_delete, function(err) {
+            if (err) {
+               throw err
+            }
+
             callback()
          })
-      })
-   }
+      }
 
-   var _delete_file = function(file_to_delete, callback) {
-      fs.unlink(FILES_DIRECTORY + file_to_delete, function(err) {
-         if (err) {
-            throw err
-         }
-
-         callback()
-      })
-   }
-
-   var FileModel = function() {
       return {
          get_all_files: _get_all_files,
          upload: _upload,
          delete_file: _delete_file
       }
    }
+
    module.exports = FileModel
 })()
