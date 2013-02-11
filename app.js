8e6dfdd2389dbbe2a015ef5bf1dd543dcef8528e
Matt Todd <matt.c.todd@gmail.com>
2013-02-10 18:57:52 -0600

    hacked something that actually works for redirecting using passport but it seems overly complicated [#1]

--- app.js
+++ app.js
@@ -2,17 +2,53 @@
    var express = require('express')
      , http = require('http')
      , path = require('path')
+     , passport = require('passport')
+     , LocalStrategy = require('passport-local').Strategy
      , files = require('./app/files')
      , main = require('./app/main')
      , login = require('./app/login')
 
    var app = express()
 
+   app.use(express.static(path.join(__dirname, 'public')))
+   app.set('view engine', 'jade')
+
    app.use(express.logger('dev'))
    app.use(express.bodyParser())
    app.use(express.methodOverride())
-   app.use(express.static(path.join(__dirname, 'public')))
-   app.set('view engine', 'jade')
+
+   app.use(express.cookieParser())
+   app.use(express.session({ secret: 'keyboard cat' }))
+   app.use(passport.initialize())
+   app.use(passport.session())
+
+   passport.serializeUser(function(user, done) {
+      done(null, user.id)
+   })
+   passport.deserializeUser(function(id, done) {
+      done(null, { id: id, name: 'Matt Todd' })
+   })
+
+   passport.use(new LocalStrategy(function(username, password, done) {
+      console.log('********************')
+      console.log('authenticating...')
+      if (username === 'user123' && password === 'password') {
+         console.log(' - good credentials')
+         done(null, {id: 123})
+      } else {
+         console.log(' - failed login')
+
+         done({ message: 'Unauthenticated user' })
+      }
+   }))
+
+   app.use(function(req, res, next) {
+      if (req.user || req.path == '/login') {
+         next()
+      } else {
+         res.redirect('/login')
+      }
+   })
 
    app.use(main)
    app.use(files)
--- app/login/index.js
+++ app/login/index.js
@@ -1,12 +1,17 @@
 (function() {
    var app = require('express')()
+   var passport = require('passport')
    app.locals.pretty = true
    app.set('views', __dirname)
 
    var controller = require('./login_controller')()
 
    app.get('/login', controller.form)
-   app.post('/login', controller.login)
+   app.post('/login',
+      passport.authenticate('local', {
+         failureRedirect: '/login',
+         successRedirect: '/'
+      }))
 
    module.exports = app
 })()
